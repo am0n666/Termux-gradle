75a0bf2028833c42cac0180772be639853d159e5
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-06 15:55:11 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,18 @@ alias ee='echo -e'
 echo
 ee "\e[93mChecking for presence of installed JDK"
 echo 
-if [ -e "$PREFIX"/bin/java ] || [ -e "$PREFIX"/share/openjdk-11.0.1/bin/java ] || [ -e "$PREFIX"/share/jdk8/bin/java ]
+if [ -e "$PREFIX"/bin/java ] || [ -e "$PREFIX"/share/jvm/openjdk-11.0.1/bin/java ] || [ -e "$PREFIX"/share/jdk8/bin/java ]
 then
-	ee "\e[32mJava is already installed!"
+	ee "\e[32mJava is installed!"
 	java --version
 	echo
-
+        ee "\e[32mInstalling Gradle... It will take some time... Have a cup of tea..☕☕"
+        ./gradlew
+else
+        ee "\e[32mJava not installed.. Cloning scripts to install JDK"
+        ee "\e[32mRe-run this script after installing JDK..."
+        git clone https://github.com/suhan-paradkar/java-in-termux.git
+        cd java-in-termux
+        chmod +x install.sh
+        ./install.sh
+fi
