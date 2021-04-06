4ef713c79cabc9bc551f269fef16d5abc69ce995
suhan-paradkar <76830230+suhan-paradkar@users.noreply.github.com>
2021-04-06 15:47:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
-cd $HOME
+shopt -s expand_aliases
+alias ee='echo -e'
+echo
+ee "\e[93mChecking for presence of installed JDK"
+echo 
+if [ -e "$PREFIX"/bin/java ] || [ -e "$PREFIX"/share/openjdk-11.0.1/bin/java ] || [ -e "$PREFIX"/share/jdk8/bin/java ]
+then
+	ee "\e[32mJava is already installed!"
+	java --version
+	echo
+
